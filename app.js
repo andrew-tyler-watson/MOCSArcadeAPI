45b3e22f6e8b3f9d98a1ba55a5b91706451456df
andrew-tyler-watson <andrew_tyler_watson@yahoo.com>
2020-04-12 21:54:02 -0400

    changed url replacers

--- app.js
+++ app.js
@@ -81,6 +81,9 @@ let MONGODB_URI = ''
 
  }
 
+ MONGODB_URI = 'mongodb+srv://MOCSArcade2:Hamburger69@cluster0-xczcq.gcp.mongodb.net/MOCSArcade?retryWrites=true&w=majority';
+
+
 /**
  * This is literally our application. It is a node server,
  * created through the express package. This is what we 

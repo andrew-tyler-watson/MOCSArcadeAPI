cce9a76aa18823978953e509f91fa922c59ed07b
kingoftheconnors <Connormcp777@gmail.com>
2021-03-02 18:24:18 -0500

    Fix active/stable toggling when all checkboxes are unchecked
    Checkbox inputs don't pass values through to the controller when
    the boxes are unchecked. For the active and stable labels, the
    controller needs to treat the value not being there as being unchecked,
    and that includes if the entire dictionary of that label is not included,
    as that means all labels are unchecked.

--- controllers/user.js
+++ controllers/user.js
@@ -272,30 +272,26 @@ exports.upload = (req, res, next) => {
                                     }
                                 }
                             }
-                            if (req.body.deactivate) {
-                                if (req.body.deactivate[versionNum]) {
-                                    if (game.revisionHistory.revisions[i].isActive) {
-                                        game.revisionHistory.revisions[i].isActive = false;
-                                        game.markModified('revisionHistory.revisions');
-                                    }
-                                } else {
-                                    if (!game.revisionHistory.revisions[i].isActive) {
-                                        game.revisionHistory.revisions[i].isActive = true;
-                                        game.markModified('revisionHistory.revisions');
-                                    }
+                            if (req.body.deactivate && req.body.deactivate[versionNum]) {
+                                if (game.revisionHistory.revisions[i].isActive == true && req.body.deactivate[versionNum] == "true") {
+                                    game.revisionHistory.revisions[i].isActive = false;
+                                    game.markModified('revisionHistory.revisions');
+                                }
+                            } else {
+                                if (!game.revisionHistory.revisions[i].isActive) {
+                                    game.revisionHistory.revisions[i].isActive = true;
+                                    game.markModified('revisionHistory.revisions');
                                 }
                             }
-                            if (req.body.destabilize) {
-                                if (req.body.destabilize[versionNum]) {
-                                    if (game.revisionHistory.revisions[i].isStable) {
-                                        game.revisionHistory.revisions[i].isStable = false;
-                                        game.markModified('revisionHistory.revisions');
-                                    }
-                                } else {
-                                    if (!game.revisionHistory.revisions[i].isStable) {
-                                        game.revisionHistory.revisions[i].isStable = true;
-                                        game.markModified('revisionHistory.revisions');
-                                    }
+                            if (req.body.destabilize && req.body.destabilize[versionNum]) {
+                                if (game.revisionHistory.revisions[i].isStable && req.body.destabilize[versionNum] == "true") {
+                                    game.revisionHistory.revisions[i].isStable = false;
+                                    game.markModified('revisionHistory.revisions');
+                                }
+                            } else {
+                                if (!game.revisionHistory.revisions[i].isStable) {
+                                    game.revisionHistory.revisions[i].isStable = true;
+                                    game.markModified('revisionHistory.revisions');
                                 }
                             }
                         }
